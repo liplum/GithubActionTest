31a52557640505d3731470e1dd11177bb33cb22c
Liplum <Li_plum@outlook.com>
2024-05-14 04:38:22 +0800

    pass context object

--- tools/contexts.js
+++ tools/contexts.js
@@ -1,5 +1,7 @@
-const envGitHub = process.env.GITHUB
+const githubEnv = process.env.GITHUB_ENV
+const githubRef = process.env.GITHUB_REF
 const paramGitHub = process.argv[2]
 
-console.log("GitHub contexts from env", envGitHub)
+console.log("GITHUB_ENV", githubEnv)
+console.log("GITHUB_REF", githubRef)
 console.log("GitHub contexts from parameter", paramGitHub)
