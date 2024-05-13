36457bc3c2294c5e6c2c1acd275e778891cdf19f
Liplum <Li_plum@outlook.com>
2024-05-14 04:40:34 +0800

    print `github` if it's an object

--- tools/contexts.js
+++ tools/contexts.js
@@ -5,3 +5,12 @@ const paramGitHub = process.argv[2]
 console.log("GITHUB_ENV", githubEnv)
 console.log("GITHUB_REF", githubRef)
 console.log("GitHub contexts from parameter", paramGitHub)
+
+if (typeof paramGitHub == "object") {
+  console.log("GitHub context is an object")
+  for (const [key, value] of Object.entries(paramGitHub)) {
+    console.log(key, value)
+  }
+} else {
+  console.log(`GitHub context is a ${typeof paramGitHub}`)
+}
